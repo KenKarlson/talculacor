a1dd1a655f514d38e58b136076b9a08ea680f9f4
KenKarlson <54804808+KenKarlson@users.noreply.github.com>
2021-07-06 19:07:51 +0300

    I added a method

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+
+
+    private static int a = 0;
+    private static int b = 0;
+    private static int e = 0;
+    private static char c;
+
     public static void main(String[] args) {
         //Ввод данных
         System.out.println("Введите необходимые числа и операцию разделяя их пробелом !");
         String insert = new Scanner(System.in).nextLine();
 
         //Обработка данных
-
         String[] newData = insert.trim().split(" ");
-        if (newData.length == 3) {
+        if (newData.length == 3 ) {
             System.out.println("OK!");
+            a = Integer.parseInt(newData[0]);
+            b = Integer.parseInt(newData[2]);
+            c = newData[1].charAt(0);
         }
-        int xxxx = 0;
-        char xx = newData[1].charAt(0);
-        switch (xx) {
+        //Вывод данных
+        System.out.println(returnResult(a,c,b));
+    }
+    public static int returnResult(int a, char c, int b){
+        switch (c) {
             case ('+'):
-                xxxx = Integer.parseInt(newData[0]) + Integer.parseInt(newData[2]);
+                e = a + b;
                 break;
             case ('-'):
-                xxxx = Integer.parseInt(newData[0]) - Integer.parseInt(newData[2]);
+                e = a - b;
                 break;
             case ('*'):
-                xxxx = Integer.parseInt(newData[0]) * Integer.parseInt(newData[2]);
+                e = a + b;
                 break;
             case ('/'):
-                xxxx = Integer.parseInt(newData[0]) / Integer.parseInt(newData[2]);
+                e = a / b;
                 break;
-
         }
-
-        //Вывод данных
-        System.out.println(xxxx);
-
-
+        return e;
     }
+
 }
